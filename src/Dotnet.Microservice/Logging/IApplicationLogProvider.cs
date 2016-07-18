d33fbf6fdb91611fe7f069b62ec0f4d6b3f1506f
Matthew Bentham <lynxx131@outlook.com>
2016-07-18 09:16:38 +0100

    Remove application logging classes as this will be replaced with liblog for our own internal needs which will allow plug and play support for most logging frameworks.

--- src/Dotnet.Microservice/Logging/IApplicationLogProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dotnet.Microservice.Logging
-{
-    public interface IApplicationLogProvider
-    {
-        void WriteLog(string message, LogLevel level);
-        bool IsEnabled(LogLevel level);
-    }
-}
